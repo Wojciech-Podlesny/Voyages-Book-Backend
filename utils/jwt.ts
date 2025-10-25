6f22cedafe436893964ae06d6c9888bc37b08a6c
wojtek <wojciech.podlesny1@gmail.com>
2025-10-25 22:17:51 +0200

    Update files jwt utils

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -1,7 +1,6 @@
 import { JWT_EXPIRES_IN, JWT_REFRESH_EXPIRES_IN, JWT_REFRESH_SECRET, JWT_SECRET } from "../config/env";
 import jwt, { SignOptions } from "jsonwebtoken";
 
-// ...existing code...
 export const generateAccesToken = (userId: string) => {
     return jwt.sign({ userId }, JWT_SECRET as string, {
         expiresIn: JWT_EXPIRES_IN as SignOptions["expiresIn"],
